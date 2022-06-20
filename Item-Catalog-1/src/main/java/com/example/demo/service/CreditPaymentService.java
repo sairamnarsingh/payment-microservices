f2a329622f51f5119848ba8d33da47696060881a
sairamnarsingh <85625848+sairamnarsingh@users.noreply.github.com>
2022-06-20 16:53:59 +0530

    Delete Item-Catalog-1 directory

--- Item-Catalog-1/src/main/java/com/example/demo/service/CreditPaymentService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.CreditAmount;
-import com.example.demo.model.CreditRequest;
-
-public interface CreditPaymentService {
-
-	public CreditAmount creditAmount(CreditRequest creditRequest);
-}
--- Item-Catalog-1/src/main/java/com/example/demo/util/RandomNoGenerator.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.util;
-
-public class RandomNoGenerator {
-
-	public static String idGen() {
-		String hexString = Long.toHexString(Double.doubleToLongBits(Math.random()));
-		return hexString;
-	}
-}
