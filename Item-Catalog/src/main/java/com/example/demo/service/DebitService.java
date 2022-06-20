3f3cbda7af6a3b5ee68391d07ae324a454966188
sairamnarsingh <85625848+sairamnarsingh@users.noreply.github.com>
2022-06-20 16:54:20 +0530

    Delete Item-Catalog directory

--- Item-Catalog/src/main/java/com/example/demo/service/DebitService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.DebitAmount;
-import com.example.demo.model.DebitRequest;
-
-public interface DebitService {
-
-	public DebitAmount debitAmount(DebitRequest debitRequest);
-}
